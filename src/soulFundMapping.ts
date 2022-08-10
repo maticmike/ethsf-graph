60c7df99d5dfb39070e76c37b197db2d42652779
ehsueh <emma.yc.hsueh@gmail.com>
2022-08-10 05:55:17 -0400

    add mvp handler (deployed to v0.4)

--- src/soulFundMapping.ts
+++ src/soulFundMapping.ts
@@ -1,12 +1,40 @@
-import { BigInt } from "@graphprotocol/graph-ts";
+import { Address } from "@graphprotocol/graph-ts";
 import {
   Transfer,
   NewWhitelistedNFT,
   VestedFundsClaimedEarly
 } from "../generated/SoulFund/SoulFund";
+import { Account, Token } from "../generated/schema";
 
-export function handleTransfer(event: Transfer): void {}
+export function handleTransfer(event: Transfer): void {
 
-export function handleNewWhitelistedNFT(event: NewWhitelistedNFT): void {}
+  // assume soulbound
+  const id = event.params.tokenId;
+  let token = new Token(id.toString());
 
-export function handleVestedFundsClaimedEarly(event: VestedFundsClaimedEarly): void {}
+  token.fund = event.address;
+
+  // receiver of token is the beneficiary
+  token.beneficiary = loadOrCreateAccount(event.params.to).id;
+
+  // granter is whoever called the contract
+  token.grantor = loadOrCreateAccount(event.transaction.from).id;
+
+}
+
+export function handleNewWhitelistedNFT(event: NewWhitelistedNFT): void {
+
+}
+
+export function handleVestedFundsClaimedEarly(event: VestedFundsClaimedEarly): void {
+
+
+}
+
+export function loadOrCreateAccount(address: Address): Account {
+  let account = Account.load(address.toHexString());
+  if (account == null) {
+    account = new Account(address.toHexString());
+  }
+  return account;
+}
